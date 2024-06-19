adcdb386d969b19647bcbe756e4477f3ed5e91c1
Christian Neri <cbneris@gmail.com>
2024-06-19 12:28:39 -0600

    Se agrega los usuarios para identificarse en la aplicación

--- src/main/java/com/example/spring/security/SecurityConfig.java
+++ src/main/java/com/example/spring/security/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,11 +33,27 @@ public class SecurityConfig {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(
 				auth -> auth
-				.requestMatchers("/loans", "/balance", "/accounts", "/cards").permitAll()
-				.anyRequest().authenticated())
-		.formLogin(Customizer.withDefaults())
-				.httpBasic(Customizer.withDefaults());
+				.requestMatchers("/loans", "/balance", "/accounts", "/cards").authenticated()
+				.anyRequest().permitAll())
+			.formLogin(Customizer.withDefaults())
+			.httpBasic(Customizer.withDefaults());
 
 		return http.build();
+	}
+	
+	@Bean
+	InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+
+		UserDetails admin = User.withUsername("admin")
+				.password("password")
+				.authorities("ADMIN")
+				.build();
+		
+		UserDetails user = User.withUsername("user")
+				.password("password")
+				.authorities("USER")
+				.build();
+		
+		return new InMemoryUserDetailsManager(admin, user);
 	}
 }
