5f5dd5ba76e3684eb6fa0af8cc8b620ca988daad
Christian Neri <cbneris@gmail.com>
2024-06-26 17:49:07 -0600

    Se agrega la entidad de roles y se modifica customer para tener muchos roles

--- src/main/java/com/example/spring/entity/CustomerEntity.java
+++ src/main/java/com/example/spring/entity/CustomerEntity.java
@@ -1,10 +1,14 @@
 package com.example.spring.entity;
 
 import java.io.Serializable;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -25,8 +29,9 @@ public class CustomerEntity implements Serializable{
 	
 	@Column(name = "pwd")
 	private String password;
-	
-	@Column(name = "rol")
-	private String role;
+
+	@OneToMany(fetch = FetchType.EAGER)
+	@JoinColumn(name = "customers")
+	private List<RolEntity> roles;
 	
 }
